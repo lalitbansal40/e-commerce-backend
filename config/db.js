41c24f9c3863c2a9921064ea6c238f2aaf7600f9
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2024-07-24 12:52:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose  from "mongoose";
 
  export const connectDb = async()=>{
-    await mongoose.connect('mongodb+srv://marketloomcustomercare:<password>@cluster0.qrimbpl.mongodb.net/marketloomcustomercare').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://marketloom:7m1QEaS2oZyLwQYL@cluster0.qrimbpl.mongodb.net/marketloomcustomercare').then(()=>console.log("DB Connected"))
 }
